cd5982950b370fb2be039dc1c617c97b5c010910
Ayc023 <Dakiesayco@gmail.com>
2024-03-08 09:56:26 +0800

    added changes

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,19 +1,37 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle,  } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
   return (
-    <IonCard>
-    <img alt="Silhouette of mountains" src="assets/img/Judging.jpg" />
-    <IonCardHeader>
-      <IonTitle>Wassup Sir</IonTitle>
-      <IonCardTitle>Card Title</IonCardTitle>
-      <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
-    </IonCardHeader>
+    
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>WASSUP SIR</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Tab 1</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+       
+        <IonCard>
+        <div style={{ textAlign: 'center' }}>
+    <img alt="Silhouette of mountains" src="assets/img/bleee.jpg" style={{ maxWidth: '100%', maxHeight: '100%', display: 'inline-block' }} />
+  </div>
+      <IonCardHeader>
+        <IonCardTitle>Emmanuel Ayco</IonCardTitle>
+        <IonCardSubtitle>Student</IonCardSubtitle>
+      </IonCardHeader>
 
-    <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
-  </IonCard>
+      <IonCardContent>“Before you criticize someone, you should walk a mile in their shoes. That way when you criticize them, you are a mile away from them and you have their shoes.”
+       —Jack Handey</IonCardContent>
+    </IonCard>
+      </IonContent>
+    </IonPage>
   );
 };
 
