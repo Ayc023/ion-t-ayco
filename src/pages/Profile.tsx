d91356409ad8603cd79a969b1b94da03a11d83aa
Ayc023 <dakiesayco@gmail.com>
2024-04-05 21:38:57 +0800

    added changes

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -44,15 +44,15 @@ const Profile: React.FC = () => {
         </IonHeader>
         
         <IonCard>
-          <img alt="Silhouette of mountains" id="cover_pic" onClick={toggleModal_coverpic} src="../src/assets/img/COVER_PIC.png" />
-          <img alt="Silhouette of mountains" id="profile_pic" onClick={toggleModal_profpic} src="../src/assets/img/PROFILE_PIC.png" />  
+          <img alt="Silhouette of mountains" id="cover_pic" onClick={toggleModal_coverpic} src="assets/img/Cover.jpg" />
+          <img alt="Silhouette of mountains" id="profile_pic" onClick={toggleModal_profpic} src="assets/img/Ayco.jpg" />  
 
           <IonCardHeader>
-            <IonCardTitle>Angela N. Libay</IonCardTitle>
+            <IonCardTitle>Emmanuel O. Ayco Jr</IonCardTitle>
             <IonCardSubtitle>BSIT Student, 3rd Year - NBSC</IonCardSubtitle>
           </IonCardHeader>
 
-          <IonCardContent><i>❤️ Blessed</i> 😊 🙏 &#x1F446;</IonCardContent>
+          <IonCardContent><i>Misfit</i>  &#x1F446;</IonCardContent>
 
           <IonButton onClick={() => setShowAlert(true)} expand="block" color="primary">Quick Facts</IonButton>
 
@@ -124,7 +124,7 @@ const Profile: React.FC = () => {
             id="modal_cover_pic"
             className="custom-modal"
           >
-            <img alt="Silhouette of mountains" id="coverpic" src="../src/assets/img/COVER_PIC.png" />
+            <img alt="Silhouette of mountains" id="coverpic" src="assets/img/Cover.jpg" />
             <IonButton color="light" id="modal_coverpic_close_button" onClick={() => setShowModal_coverpic(false)}>X</IonButton>
           </IonModal>
           
@@ -135,7 +135,7 @@ const Profile: React.FC = () => {
             className="custom-modal"
           >
             {/* <img alt="Silhouette of mountains" src="../src/assets/img/HENHEN_JPG.jpg" /> */}
-            <img alt="Silhouette of mountains" id="profpic" src="../src/assets/img/PROFILE_PIC.png" />
+            <img alt="Silhouette of mountains" id="profpic" src="assets/img/Ayco.jpg" />
             <IonButton color="light" id="modal_profpic_close_button" onClick={() => setShowModal_profpic(false)}>X</IonButton>
           </IonModal>
 
