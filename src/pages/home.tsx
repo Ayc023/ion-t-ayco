9aa3a37bd1fd3e75d4696cdb9ee0032dd4dafe6d
Ayc023 <Dakiesayco@gmail.com>
2024-05-10 09:08:23 +0800

    added changes

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -19,7 +19,7 @@ const Home: React.FC = () => {
   const filteredCards = [
     { id: 'card1', title: 'Click counter', imageUrl: '../src/assets/img/Click Counter.gif', onClick: () => history.push('/click_counter') },
     { id: 'card2', title: 'Calculator', imageUrl: '../src/assets/img/Calculator.gif', onClick: () => history.push('/calculator') },
-    { id: 'card3', title: 'Todo List', imageUrl: '../src/assets/img/Todolist.gif', onClick: () => history.push('/to_do_list') },
+    { id: 'card3', title: 'Todolist', imageUrl: '../src/assets/img/Todolist.gif', onClick: () => history.push('/todolist') },
     { id: 'card4', title: 'Qoutes Generator', imageUrl: '../src/assets/img/Quote_Generator.gif', onClick: () => history.push('/qoutes_generator') },
     { id: 'card5', title: 'Notes', imageUrl: '../src/assets/img/notes_icon.png', onClick: () => history.push('/notes') },
   ].filter(card => card.title.toLowerCase().includes(searchQuery.toLowerCase()));
